2e05bb30eda01f97239425a5e0f06753a24ebe22
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2015-12-20 08:48:45 +0900

    translation 1-1 Crazy.java JPNcomments

--- sample/Crazy.java
+++ sample/Crazy.java
@@ -4,14 +4,17 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://robocode.sourceforge.net/license/epl-v10.html
+ *所謂コピーライト
  */
 package sample;
 
+//Crazy.javaが収納されているpackage名
 
 import robocode.*;
 
 import java.awt.*;
 
+//Crazy.javaで使用するためにimportしてきたクラス
 
 /**
  * Crazy - a sample robot by Mathew Nelson.
