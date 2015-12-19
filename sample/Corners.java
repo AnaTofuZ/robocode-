4bb385fdb63b71a800c250136b25d7bb2d330fe2
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2015-12-19 16:06:03 +0900

    add corners.java JPN

--- sample/Corners.java
+++ sample/Corners.java
@@ -4,6 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://robocode.sourceforge.net/license/epl-v10.html
+ * 所謂コピーライトの表記。特に意味は無い
  */
 package sample;
 
@@ -13,80 +14,101 @@ import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-import java.awt.*;
+//主要メソッドをimportする
 
+import java.awt.*;
 
 /**
  * Corners - a sample robot by Mathew Nelson.
  * <p/>
  * This robot moves to a corner, then swings the gun back and forth.
- * If it dies, it tries a new corner in the next round.
+ *このロボットは角へ移動し，主砲を前後あちらこちらに動かす
  *
+ * If it dies, it tries a new corner in the next round.
+ *もしこのロボット自身が撃沈した場合，新しい角から次のラウンドを始める
  * @author Mathew A. Nelson (original)
  * @author Flemming N. Larsen (contributor)
  */
+
 public class Corners extends Robot {
 	int others; // Number of other robots in the game
-	static int corner = 0; // Which corner we are currently using
-	// static so that it keeps it between rounds.
-	boolean stopWhenSeeRobot = false; // See goCorner()
+	//このゲームで他のロボットの数を把握する
+	static int corner = 0; // staticなint型変数cornerを宣言し0を代入
+	// staticはround間で保存される
+	boolean stopWhenSeeRobot = false; //boolean型変数stopWhenSeeRobotを宣言し，falseを代入
+	// この一行がどう活用されるかはgo Corner()を参照
 
 	/**
 	 * run:  Corners' main run function.
+	 * runメソッド: Cornerのmainとなるrunメソッド
 	 */
+
 	public void run() {
-		// Set colors
+		// 機体の色を設定
 		setBodyColor(Color.red);
 		setGunColor(Color.black);
 		setRadarColor(Color.yellow);
 		setBulletColor(Color.green);
 		setScanColor(Color.green);
 
-		// Save # of other bots
+		// robocode.Robotのメソッド，getOthersを用いて残っている敵の数をotherに渡す
 		others = getOthers();
 
-		// Move to a corner
+		// 角へ移動する
 		goCorner();
 
-		// Initialize gun turn speed to 3
+		// 大砲の回転速度を3に初期化
 		int gunIncrement = 3;
+		//ここではint型変数gunIncrementを宣言し，そこに3を代入している
 
-		// Spin gun back and forth
+		// 大砲を行ったり来たり回転させる
 		while (true) {
-			for (int i = 0; i < 30; i++) {
-				turnGunLeft(gunIncrement);
+			for (int i = 0; i < 30; i++) { //カウンタ変数iが30になるまでループ
+				turnGunLeft(gunIncrement);//先ほど宣言したgunIncrementをturnGunLeftに渡す。
+			//此処では速さ3
 			}
-			gunIncrement *= -1;
+			gunIncrement *= -1; //gunIncrementの値に-1をかける。これで逆向きに回転する。
 		}
 	}
 
 	/**
-	 * goCorner:  A very inefficient way to get to a corner.  Can you do better?
+	 * goCorner: これはとても効率の悪い角へ向かうメソッドである。もっと良くしてください
 	 */
 	public void goCorner() {
-		// We don't want to stop when we're just turning...
+		// 回転している時は自機は止まらない
 		stopWhenSeeRobot = false;
-		// turn to face the wall to the "right" of our desired corner.
+		// 回転時に正面を向いた壁の右側へと移動する
 		turnRight(normalRelativeAngleDegrees(corner - getHeading()));
-		// Ok, now we don't want to crash into any robot in our way...
+
+		/*RobotクラスのturnRightメソッド(機体を右回転する)を使用
+		 * turnRIghtに渡す値を正規化するためにnormalRelativeAngleDegreesメソッドを通して
+		 *現在機体が向いている座標をgetHeadingメソッド取得し
+		 *cornerからこれを引いた分の角度回転する
+		 *turnRightメソッドは処理が終了するまで戻らないので回転中は自機は何もしない*/
+
+		//stopWhenSeeRobot 変数をtrueにする
+
 		stopWhenSeeRobot = true;
-		// Move to that wall
+
+		//5000pixel分直進する。先ほど回転したので壁に当たる.
+		//Robot内のaheadメソッドは壁に衝突すると動作を完了するので，衝突後turnLeftの動きに移行する
 		ahead(5000);
-		// Turn to face the corner
+		// 角に正面を向く
 		turnLeft(90);
-		// Move to the corner
+		// 角へと移動する
 		ahead(5000);
-		// Turn gun to starting point
+		// 左に90度移動して動作完了
 		turnGunLeft(90);
 	}
 
 	/**
-	 * onScannedRobot:  Stop and fire!
+	 * onScannedRobotメソッド:  静止して砲撃
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Should we stop, or just fire?
+		// 止まる動作を含むか，止まらずに撃つかの判断
 		if (stopWhenSeeRobot) {
-			// Stop everything!  You can safely call stop multiple times.
+			//stopWhenSeeRobotがtrueだった時。すなわちgoCorner()で回転が終了した際は
+			// stopメソッドを用いて動作を全て終了
 			stop();
 			// Call our custom firing method
 			smartFire(e.getDistance());
