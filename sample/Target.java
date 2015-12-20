060203c6d86b7679d7c27c6bc951ac9f1e4b3b77
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2015-12-21 05:30:17 +0900

    finished Target.java for translated comments for JPN

--- sample/Target.java
+++ sample/Target.java
@@ -4,6 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://robocode.sourceforge.net/license/epl-v10.html
+ * 所謂コピーライト
  */
 package sample;
 
@@ -14,52 +15,58 @@ import robocode.CustomEvent;
 
 import java.awt.*;
 
+//Target.javaを構成する上で必要なクラスをimport
 
 /**
  * Target - a sample robot by Mathew Nelson.
  * <p/>
- * Sits still. Moves every time energy drops by 20.
- * This Robot demonstrates custom events.
+ * じっと座っている。エネルギーを20失った場合動く
+ * このロボットはカスタムイベントのデモンストレーションである
  *
  * @author Mathew A. Nelson (original)
  * @author Flemming N. Larsen (contributor)
  */
-public class Target extends AdvancedRobot {
+public class Target extends AdvancedRobot { //AdvancedRobotから拡張
 
-	int trigger; // Keeps track of when to move
+	int trigger; //int型変数を宣言 
 
 	/**
-	 * TrackFire's run method
+	 * TrackFire's run メソッド
 	 */
 	public void run() {
-		// Set colors
+		//色の設定
+		//機体:白,砲身:白,レーダー:白 
 		setBodyColor(Color.white);
 		setGunColor(Color.white);
 		setRadarColor(Color.white);
 
-		// Initially, we'll move when life hits 80
+		//最初，自機が動く際はエネルギーが80の時 
 		trigger = 80;
-		// Add a custom event named "trigger hit",
+		//"trigger hit "という名前のカスタムイベントを生成 
 		addCustomEvent(new Condition("triggerhit") {
 			public boolean test() {
 				return (getEnergy() <= trigger);
+		//エネルギーを確認し，triggerの値以下ならtrueを返す
 			}
 		});
 	}
 
 	/**
-	 * onCustomEvent handler
+	 * カスタムイベント発生時に呼び出されるメソッド
 	 */
 	public void onCustomEvent(CustomEvent e) {
-		// If our custom event "triggerhit" went off,
-		if (e.getCondition().getName().equals("triggerhit")) {
-			// Adjust the trigger value, or
-			// else the event will fire again and again and again...
+		//このカスタムイベントはtrigger hitと呼ばれる 
+		if (e.getCondition().getName().equals("triggerhit")) { 
+			//もし発生したコンディションがtriggerhitと呼ばれるものなら
+			// triggerの値を調整するか
+			// 砲撃イベントを何度も行う
 			trigger -= 20;
+			//triggerの値から-20引く。
+
 			out.println("Ouch, down to " + (int) (getEnergy() + .5) + " energy.");
-			// move around a bit.
-			turnLeft(65);
-			ahead(100);
+			// getEnergyで入手した数値+0.5を現在のエネルギーとして出力
+			turnLeft(65); //左65度回転
+			ahead(100); //100ピクセル直進
 		}
 	}
 }
