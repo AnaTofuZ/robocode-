e706860acc0c1f044aad3d8954d2a6f0934e1aa4
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2015-12-20 19:24:37 +0900

    finished MyFirestRobot.java translated comment JPN

--- sample/MyFirstRobot.java
+++ sample/MyFirstRobot.java
@@ -4,6 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://robocode.sourceforge.net/license/epl-v10.html
+ * 所謂コピーライト
  */
 package sample;
 
@@ -12,42 +13,43 @@ import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
+//MyFirstRobotを構成する上で使用するメソッドが収納されているクラスをimport
 
 /**
  * MyFirstRobot - a sample robot by Mathew Nelson.
  * <p/>
- * Moves in a seesaw motion, and spins the gun around at each end.
+ * シーソーの様な動きを行う。各メソッドの最後に砲身を回転させる 
  *
  * @author Mathew A. Nelson (original)
  */
-public class MyFirstRobot extends Robot {
+public class MyFirstRobot extends Robot { //Robotから継承
 
 	/**
-	 * MyFirstRobot's run method - Seesaw
+	 * MyFirstRobot's run メソッド - シーソーみたいな動き
 	 */
 	public void run() {
 
 		while (true) {
-			ahead(100); // Move ahead 100
-			turnGunRight(360); // Spin gun around
-			back(100); // Move back 100
-			turnGunRight(360); // Spin gun around
+			ahead(100); // 100ピクセル前進
+			turnGunRight(360); // 砲身を右に360度回転
+			back(100); // 100ピクセル後進
+			turnGunRight(360); // 砲身を360度右回転
 		}
 	}
 
 	/**
-	 * Fire when we see a robot
+	 *敵機を見つけた場合攻撃 
 	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
+	public void onScannedRobot(ScannedRobotEvent e) { //onScannnedRobotをオーバーライド
+		fire(1); //威力1で攻撃
 	}
 
 	/**
-	 * We were hit!  Turn perpendicular to the bullet,
-	 * so our seesaw might avoid a future shot.
+	 *もし被弾した場合，弾丸と直角に回転 
+	 *おそらくこのシーソーの動きをすれば永久的に避けることができるだろう 
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		turnLeft(90 - e.getBearing());
+		turnLeft(90 - e.getBearing());//被弾時の位置を90度から引いて，左回転
 	}
 }												
 
